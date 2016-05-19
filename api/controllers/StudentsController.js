c3aa58620a1724b34a9875412f23e06012218d56
code-for-coffee <jtraverjr@gmail.com>
2016-05-19 18:48:32 -0500

    student model schema def

---
+++ api/controllers/StudentsController.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Controller = require('trails-controller')
+
+/**
+ * @module StudentsController
+ * @description Generated Trails.js Controller.
+ */
+module.exports = class StudentsController extends Controller{
+
+}
+
--- api/models/Students.js
+++ api/models/Students.js
@@ -9,9 +9,125 @@ const Model = require('trails-model')
 module.exports = class Students extends Model {
 
   static config () {
+    return {
+      // Collection name
+      tableName: 'students',
+
+      // Schema options
+      schema: {
+        timestamps: true,
+
+        versionKey: false,
+
+        toObject: {
+          virtuals: true
+        },
+
+        toJSON: {
+          virtuals: true
+        }
+      },
+      // Schema statics
+      statics: {
+        getByEmail: function (email) {
+          return this
+            .findOne({
+              email: _.trim(email)
+            })
+            .exec()
+        },
+      },
+
+      // Schema methods
+      methods: {
+        toJSON: function () {
+          const user = this.toObject()
+          delete user.password
+
+          return user
+        }
+      }
+    }
+  }
+  /**
+   * After Trails.js will create model Schema you could add anything you want here
+   * @param  {mongoose.Schema} schema mongoose new Schema object
+   */
+  static onSchema (schema) {
+    // virtuals
+    schema.virtual('name.full').get(function () {
+      return this.name.first + ' ' + this.name.last
+    })
+    // lifecircle events
+    schema.pre('save', function (next) {
+      // performing actions
+      next()
+    })
   }
 
-  static schema () {
-
+  static Schema () {
+    return {
+      username: String,
+      childs: [{
+        type: Schema.ObjectId,
+        ref: 'StudentSchema'
+      }],
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: {
+          validator: function (val) {
+            return isEmail(val)
+          },
+          message: '{VALUE} is not a valid email'
+        }
+      },
+      password: {
+        type: String,
+        required: true,
+        validate: {
+          validator: function (val) {
+            if (val.length > 12 && typeof val === 'string')
+            return val
+          },
+          message: '{VALUE} must be at least 12 characters long'
+        }
+      },
+      firstName: {
+        type: String,
+        required: true,
+        validate: {
+          validator: function (val) {
+            if (val.length > 2 && typeof val === 'string')
+              return val
+          },
+          message: '{VALUE} must have at least 2 characters'
+        }
+      },
+      lastName: {
+        type: String,
+        required: true,
+        validate: {
+          validator: function (val) {
+            if (val.length > 2 && typeof val === 'string')
+              return val
+          },
+          message: '{VALUE} must have at least 2 characters'
+        }
+      },
+      location: {
+        type: String,
+        validate: {
+          validator: function (val) {
+            if (val.length > 2 && typeof val === 'string')
+              return val
+          },
+          message: '{VALUE} must have at least 2 characters'
+        }
+      }
+    }
   }
 }
