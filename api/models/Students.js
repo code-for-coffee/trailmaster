8cd7ac8c65721179c8a5f6a77a3be17d07c89d28
code-for-coffee <jtraverjr@gmail.com>
2016-05-19 18:36:49 -0500

    push

---
+++ api/models/Students.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const Model = require('trails-model')
+
+/**
+ * @module Students
+ * @description Student object.
+ */
+module.exports = class Students extends Model {
+
+  static config () {
+  }
+
+  static schema () {
+
+  }
+}
---
+++ api/models/index.js
@@ -0,0 +1,2 @@
+'use strict'
+exports.Students = require('./Students')
